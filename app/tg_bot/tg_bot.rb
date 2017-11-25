b80f85d005a57fa138befa29f1956608a5129839
pavel <pavel.triandafilov@castle.co>
2017-11-25 17:59:50 +0400

    tg subscrive logic

--- app/tg_bot/tg_bot.rb
+++ app/tg_bot/tg_bot.rb
@@ -1,17 +1,34 @@
 require 'telegram/bot'
+require 'pry'
 token = '469426190:AAEmNc3nBLSzAxemTs4ovvTccjM-MSwPUHI'
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
+
+    answers = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: [%w(Подписаться Отписаться)], one_time_keyboard: true)
+
     case message.text
       when '/start'
-        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}. My bot will trade on livecoin and inform you.\nThe initial data is: btc = 0.00186976")
+        bot.api.send_message(chat_id: message.chat.id,
+                             text: "Hello, #{message.from.first_name}. My bot will trade on livecoin and inform you.\n Wanna susbscribe?",
+                             reply_markup: answers
+        )
 
+      when 'Подписаться'
         f = File.open("../../users", 'a')
         f << "#{message.chat.id}\n"
         f.close
 
         puts "Described #{message.from.first_name}"
+
+      when 'Отписаться'
+        f = File.read("../../users")
+        f.gsub!("#{message.chat.id}\n", "")
+
+        File.open("../../users", "w") {|file| file.puts f }
+
+        puts "Unsubscribed #{message.from.first_name}"
+
     end
   end
 end
